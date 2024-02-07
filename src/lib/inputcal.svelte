e2bed11a54fa8ade608129bdbeab839c87889f77
Kyra Mo <kyraymo@gmail.com>
2024-02-06 22:25:20 -0500

    style changes for merge

---
+++ src/lib/inputcal.svelte
@@ -0,0 +1,33 @@
+<script lang="ts"> 
+    import Calendar from '@event-calendar/core';
+    import TimeGrid from '@event-calendar/time-grid';
+    let ec;
+    let plugins = [TimeGrid];
+    let options = {
+        hiddenDays: [1, 2, 4, 6],
+        view: 'timeGridWeek',
+        allDaySlot: false,
+        headerToolbar: {start: '', center: 'title', end: ''},
+        date: '2024-02-07',
+        slotMaxTime: '21:00:00',
+        slotMinTime: '8:00:00',
+        selectable: true
+    };
+</script>
+
+<h3>color in your availability</h3>
+<Calendar bind:this={ec} {options} {plugins}/>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans:wght@200;300&display=swap');
+    h3 {
+        text-align: center;
+        font-family: 'Noto Sans', sans-serif;
+      }
+      
+    :global(.ec){
+        max-width: 30vw;
+        margin: 40px auto;
+        font-family: 'Noto Sans', sans-serif;
+      }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,7 @@
-<script lang="ts"> 
-    import Calendar from '@event-calendar/core';
-    import TimeGrid from '@event-calendar/time-grid';
-    let ec;
-    let plugins = [TimeGrid];
-    let options = {
-        hiddenDays: [1, 2, 6],
-        view: 'timeGridWeek',
-        allDaySlot: false,
-        headerToolbar: {start: '', center: 'title', end: ''},
-        date: '2024-02-07',
-        slotMaxTime: '21:00:00',
-        slotMinTime: '8:00:00',
-        selectable: true
-    };
+<script>
+    import InputCal from '../lib/inputcal.svelte';
 </script>
 
-<h3>color in the lines!</h3>
-<p>the color part is still loading</p>
-<Calendar bind:this={ec} {options} {plugins}/>
-
-<style>
-    h3 {
-        font-family: 'Courier New', Courier, monospace;
-        text-align: center
-      }
-      
-    :global(.ec){
-        max-width: 1100px;
-        margin: 40px auto;
-        font-family: 'Courier New', Courier, monospace;
-      }
-</style>
+<main>
+  <InputCal />
+</main>
